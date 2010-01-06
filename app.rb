d7bfee7b9117c2a3f265e7eeebb60492515686db
Matt Mitchell <goodieboy@gmail.com>
2010-01-06 06:32:59 -0500

    added named routes and updated "controllers"

--- app.rb
+++ app.rb
@@ -61,21 +61,28 @@ helpers do
     @openid_consumer ||= OpenID::Consumer.new(session, OpenID::Store::Filesystem.new("#{File.dirname(__FILE__)}/tmp/openid"))
   end
   
+  map(:root).to '/'
+  map(:sessions).to '/sessions'
+  map(:new_session).to '/sessions/new'
+  map(:posts).to '/posts'
+  map(:new_post).to '/posts/new'
+  map(:edit_post).to '/posts/:id/edit'
+  map(:post).to '/posts/:id'
+  
 end
 
-get '/sessions/new' do
+get :new_session do
   erb :'/sessions/new'
 end
 
-post '/sessions' do
+post :sessions do
   if resp = request.env["rack.openid.response"]
-    puts 'response...'
     if resp.status == :success
      flash[:notice] = "Login successful!"
-     redirect '/'
+     redirect url_for(:root)
     else
       flash[:notice] = "Error: #{resp.status}"
-      redirect '/sessions/new'
+      redirect url_for(:new_session)
     end
   else
     www_auth_headers = Rack::OpenID.build_header(:identifier => params[:openid_url])
@@ -84,47 +91,44 @@ post '/sessions' do
   end
 end
 
-delete '/sessions' do
+delete :sessions do
   session.clear
-  redirect '/'
+  redirect url_for(:root)
 end
 
 # index
-get '/' do
+get :root do
   list_posts
 end
 
 # index
-get '/posts' do
+get :posts do
   list_posts
 end
 
 # new
-get '/posts/new' do
+get :new_post do
   @post = Post.new(params[:post] || {})
   erb :'posts/new'
 end
 
 # create
-post '/posts' do
-  doc = Post.new({})
-  doc.id = Time.now.to_i
-  doc.title = params[:post][:title]
-  doc.body = params[:post][:body]
+post :posts do
+  doc = Post.new(:id => Time.now.to_i, :title => params[:post][:title], :body => params[:post][:body])
   solr.add doc
-  solr.commit.to_s
-  redirect "/posts/#{doc.id }"
+  solr.commit
+  redirect url_for(:post, :id => doc.id)
 end
 
 # show
-get '/posts/:id' do
+get :post do
   @sresponse = Post.find_by_id params[:id]
   @post = @sresponse.docs.first
   erb :'posts/show'
 end
 
 # edit
-get '/posts/:id/edit' do
+get :edit_post do
   @sresponse = Post.find_by_id params[:id]
   @post = @sresponse.docs.first
   erb :'posts/edit'
@@ -133,12 +137,10 @@ end
 # update
 put '/posts/:id' do
   @sresponse = Post.find_by_id params[:id]
-  doc = @sresponse.docs.first
-  doc.title = params[:post][:title]
-  doc.body = params[:post][:body]
+  doc = @sresponse.docs.first.merge(:title => params[:post][:title], :body => params[:post][:body])
   solr.add doc
   solr.commit
-  redirect "/posts/#{params[:id]}"
+  redirect url_for(:post, :id => params[:id])
 end
 
 # delete
@@ -147,5 +149,5 @@ delete '/posts/:id' do
   doc = @sresponse.docs.first
   solr.delete_by_query %(id:"#{doc.id}")
   solr.commit
-  redirect "/posts"
+  redirect url_for(:posts)
 end
